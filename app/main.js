7120739d7bcf94db9bb0b01e8c3feff635e0cf25
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-07-28 15:29:29 +0530

    Fixed uwu

--- app/main.js
+++ app/main.js
@@ -4,16 +4,19 @@ let od,no,length;
 
 $("#od_of_pipe").change(function() {
     od = $("#od_of_pipe").val();
+    console.log("TCL: od", od)
     calulate();
 });
 
 $("#length_of_pipe").change(function() {
     length = $("#length_of_pipe").val();
+    console.log("TCL: length", length)
     calulate();
 });
 
 $("#no_of_pipe").change(function() {
     no = $("#no_of_pipe").val();
+    console.log("TCL: no", no)
     calulate();
 });
 
@@ -25,11 +28,12 @@ function calulate() {
         parseFloat(length);
         parseFloat(no);
     } catch (err) {
-        return $("#capcaity").attr("value", "Enter numbers.");
+        return $("#capacity").attr("value", "Enter numbers.");
     }
     if(od < 0 || length < 0 || no < 0) {
-        return $("#capcaity").attr("value", "Enter positive numbers please.");
+        return $("#capacity").attr("value", "Enter positive numbers please.");
     }
         let result = (((od * Math.PI) + ((od * Math.PI) * length) + (((od * Math.PI) * length) * no)) / 1000000);
-        return $("#capcaity").attr("value", result);
-}
+        console.log("TCL: calulate -> result", result)
+        return $("#capacity").attr("value", "~" + result.toFixed());
+} // Ha
